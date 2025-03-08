c4320066d69dad46950f8026449dc20bb433b7ba
samuelson13 <samucruzvk@gmail.com>
2025-03-08 10:04:50 +0000

    coloquei nota de ajuda

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Tema 1 - Cadastro das Carta
 
 int main() {
 
-   printf("  Carta 1:\n\nEstado: A\nCódigo: A01\n");
-   printf("Nome da Cidade: São Paulo \nPopulação: 12325000\n");
-   printf("Área: 1521.11 km \nPIB: 699.28 bilhões de reais\n");
-   printf("Número de Pontos Turísticos: 50\n\n");
+    printf("  Carta 1:\n\nEstado: A\nCódigo: A01\n");
+    printf("Nome da Cidade: São Paulo \nPopulação: 12325000\n");
+    printf("Área: 1521.11 km \nPIB: 699.28 bilhões de reais\n");
+    printf("Número de Pontos Turísticos: 50\n\n");
   
-   printf(" Carta 2:\n\nEstado: B\nCódigo: A02\nNome da Cidade: Rio de Janeiro\nPopulação: 12345600\nÁrea: 1521.12 Km\nPIB: 699.29 bilhões de reais\nNúmero de Pontos turisdticos 51");
+    printf("  Carta 2:\n\nEstado: B\nCódigo: A02\n");
+    printf("Nome da Cidade: Rio de Janeiro\nPopulação: 12345600\n");
+    printf("Área: 1521.12 Km\nPIB: 699.29 bilhões de reais\n");
+    printf("Número de Pontos turisdticos 51");
    
    
-   // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     // Cadastro das Cartas:
---
+++ caratasSupertrunfo2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main () {
+    int idade;
+    char cidade[20];
+
+      printf( " Digite sua idade:\n");
+     scanf("%d", &idade);
+    printf("Sua idade é: %d\n", idade);
+      printf("cidade\n");
+     scanf("%s",&cidade);
+    printf("voce e natural de:%s\n",cidade);
+return 0;
+ }
